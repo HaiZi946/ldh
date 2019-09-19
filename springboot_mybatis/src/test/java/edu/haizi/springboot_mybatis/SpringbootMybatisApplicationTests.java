d22edbe5f7a86d2c232c55fd86306dd20e6d999d
haizi <1106363444@qq.com>
2019-09-19 18:34:25 +0800

    添加junit整合

--- springboot_mybatis/src/test/java/edu/haizi/springboot_mybatis/SpringbootMybatisApplicationTests.java
+++ springboot_mybatis/src/test/java/edu/haizi/springboot_mybatis/SpringbootMybatisApplicationTests.java
@@ -1,16 +1,26 @@
 package edu.haizi.springboot_mybatis;
 
+import edu.haizi.springboot_mybatis.domain.User;
+import edu.haizi.springboot_mybatis.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringbootMybatisApplication.class)
 public class SpringbootMybatisApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
     @Test
     public void contextLoads() {
+
+        List<User> users = userMapper.queryUserList();
+        System.out.println(users);
     }
 
 }
